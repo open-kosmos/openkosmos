c91c5f1e24bf0cfc747eca6036ba89876128cc0a
Bill Meabrod <BillMeabrod@gmail.com>
2024-06-20 21:41:43 -0400

    Updating camera system based on feedback in PR

--- Assets/Scripts/Prototype/FlightControl/Systems/CameraSystem.cs
+++ Assets/Scripts/Prototype/FlightControl/Systems/CameraSystem.cs
@@ -1,3 +1,5 @@
+using Assets.Scripts.Prototype.FlightControl.Components;
+using Kosmos.FloatingOrigin;
 using Kosmos.Prototype.Parts.Components;
 using Unity.Entities;
 using Unity.Transforms;
@@ -5,15 +7,23 @@ using UnityEngine;
 
 public partial class CameraSystem : SystemBase
 {
+    private SystemHandle _cameraSystem;
+
+    protected override void OnCreate()
+    {
+        RequireForUpdate<PlayerControlledTag>();
+        _cameraSystem = World.GetExistingSystem<CameraSystem>();
+    }
+
     protected override void OnUpdate()
     {
-        var camera = GameObject.FindGameObjectWithTag("MainCamera");
-        Vector3 cameraOffset = new Vector3(0, 12, -60);
+        var cameraData = EntityManager.GetComponentData<FlightControlCameraData>(_cameraSystem);
+        var cameraTransform = cameraData.Camera.transform;
 
         Entities.ForEach((ref Entity entity, ref ControlPod controlPod, ref LocalTransform localTransform, in PlayerControlledTag playerControlledTag) =>
         {
-            camera.transform.position =
-                new Vector3(localTransform.Position.x, localTransform.Position.y, localTransform.Position.z) + cameraOffset;
+            cameraTransform.position =
+                new Vector3(localTransform.Position.x, localTransform.Position.y, localTransform.Position.z) + cameraData.Offset;
         })
         .WithoutBurst()
         .Run();
