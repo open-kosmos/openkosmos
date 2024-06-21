dfbb3f57425f6f53eb8a830c0edf837752bf0984
Bill Meabrod <BillMeabrod@gmail.com>
2024-06-20 22:11:54 -0400

    Minor change based on feedback.

--- Assets/Scripts/Prototype/FlightControl/Systems/CameraSystem.cs
+++ Assets/Scripts/Prototype/FlightControl/Systems/CameraSystem.cs
@@ -1,5 +1,4 @@
 using Assets.Scripts.Prototype.FlightControl.Components;
-using Kosmos.FloatingOrigin;
 using Kosmos.Prototype.Parts.Components;
 using Unity.Entities;
 using Unity.Transforms;
@@ -7,17 +6,15 @@ using UnityEngine;
 
 public partial class CameraSystem : SystemBase
 {
-    private SystemHandle _cameraSystem;
 
     protected override void OnCreate()
     {
         RequireForUpdate<PlayerControlledTag>();
-        _cameraSystem = World.GetExistingSystem<CameraSystem>();
     }
 
     protected override void OnUpdate()
     {
-        var cameraData = EntityManager.GetComponentData<FlightControlCameraData>(_cameraSystem);
+        var cameraData = EntityManager.GetComponentData<FlightControlCameraData>(SystemHandle);
         var cameraTransform = cameraData.Camera.transform;
 
         Entities.ForEach((ref Entity entity, ref ControlPod controlPod, ref LocalTransform localTransform, in PlayerControlledTag playerControlledTag) =>
